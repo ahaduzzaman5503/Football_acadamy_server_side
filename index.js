6d065ea3c72803c81f972c452f8ccfe1ed0f6a63
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-06-14 15:57:44 +0600

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -9,6 +10,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, massage: 'unauthorize access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, massage: 'unauthorize access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const uri =
@@ -86,8 +102,21 @@ async function run() {
           res.send(result)
         } )
 
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+          const email = req.params.email;
+          const query = {email: email}
+          if(req.decoded.email !== email){
+            res.send({ admin: false})
+          }
+          const user = await  allUsersCollection.findOne(query);
+          const result = {admin: user?.role === 'admin'}
+          res.send(result);
+        })
+
         app.post('/jwt', (req, res)=> {
           const user = req.body;
+          console.log(user);
+          // console.log({jwt: process.env.ACCESS_TOKEN_SECRET});
           const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
             expiresIn: '1h'})
             res.send({ token })
@@ -121,8 +150,12 @@ async function run() {
           res.send(result);
         });
 
-        app.get("/selectclass", async (req, res) => {
+        app.get("/selectclass", verifyJWT, async (req, res) => {
           const cursor = SelectClassCollection.find();
+          const decodedEmail = req.decoded.email;
+          if(email !== decodedEmail){
+            return res.status(401).send({error: true, massage: 'Porviden '});
+          }
           const result = await cursor.toArray();
           res.send(result);
         });
